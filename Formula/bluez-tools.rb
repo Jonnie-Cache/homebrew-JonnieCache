782d7de4930b7e20ecb2a85348ecc9a8028982cf
Jonnie-Cache <33103426+Jonnie-Cache@users.noreply.github.com>
2018-08-11 17:07:07 -0500

    Bug fix

--- Formula/bluez-tools.rb
+++ Formula/bluez-tools.rb
@@ -1,4 +1,4 @@
-class bluez-tools < Formula
+class blueztools < Formula
   desc "Automatic configure script builder"
   homepage "https://github.com/khvzak/bluez-tools"
   url "https://github.com/khvzak/bluez-tools.git"
